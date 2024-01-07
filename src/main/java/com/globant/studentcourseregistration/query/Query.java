df655e77165e5a0e837cf13e6ce7c9b168d9e9cc
Alejandro Rojas <alejandro.rojas@globant.com>
2024-01-07 15:09:11 -0600

    Activating the GraphIQL endpoint to consume this endpoint in web browser. Adding the first and second queries. Just returning a String response. Updating the annotation @Component to @Controller. It's necessary to run this endpoint. Adding annotation @QueryMapping in order to show the endpoints first and second Query.

--- src/main/java/com/globant/studentcourseregistration/query/Query.java
+++ src/main/java/com/globant/studentcourseregistration/query/Query.java
@@ -1,13 +1,21 @@
 package com.globant.studentcourseregistration.query;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
-import org.springframework.stereotype.Component;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 
-@Component
+@Controller
 public class Query implements GraphQLQueryResolver {
+
+  @QueryMapping
   public String firstQuery() {
     return "First Query";
+  }
+
+  @QueryMapping
+  public String secondQuery() {
+    return "Second Query";
   }
 
 }
