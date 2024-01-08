403070334184c150440970f02f83ea88314499eb
Alejandro Rojas <alejandro.rojas@globant.com>
2024-01-08 11:22:40 -0600

    Adding new Query that receives input arguments.

--- src/main/java/com/globant/studentcourseregistration/query/Query.java
+++ src/main/java/com/globant/studentcourseregistration/query/Query.java
@@ -1,6 +1,7 @@
 package com.globant.studentcourseregistration.query;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -16,6 +17,11 @@ public class Query implements GraphQLQueryResolver {
   @QueryMapping
   public String secondQuery() {
     return "Second Query";
+  }
+
+  @QueryMapping
+  public String fullName(@Argument String firstName, @Argument String lastName) {
+    return firstName + " " + lastName;
   }
 
 }
